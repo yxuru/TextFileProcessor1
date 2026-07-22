73af57d4abd786968ab103d91b753e547fee8a37
yxuru <haynescamden@gmail.com>
2026-07-21 20:38:09 -0400

    edits w/ future comments

--- TextFileProcessor1/TextFileProcessor1.cpp
+++ TextFileProcessor1/TextFileProcessor1.cpp
@@ -31,24 +31,47 @@ int main()
 
             std::getline(ss, left_eq, '=');
             std::getline(ss, right_eq);
+
             left_eq = trim(left_eq);
             right_eq = trim(right_eq);
 
-            data[left_eq] = right_eq;
+            data[left_eq] = right_eq;                                       // Somewhere in here have a duplicate key detector!       
 
         }
+        std::cout << "Successfully processed the text file.\n";
+
+        // Loaded ____ entries in ___ ms!
+
     }
     else
     {
         std::cout << "ERROR! NO MATCHING FILE NAME FOUND!";
+        infile.close();
+        return 0;
     }
 
-    std::cout << data["PLAYER_NAME"];
-    std::cout << data["PLAYER_GREETING"];
+    
+    
 
-    
-    findAll(data);
-    
+    while (true)
+    {
+        int desiredFunction;
+
+        std::cout << "What would you like to do?\n1. Find all items in a key\n2. Quit Program\n";   // Have a key prefix searcher and/or a partial match search
+        std::cin >> desiredFunction;                                                                // export results into a txt file , allow edits to save here
+                                                                                                    // Allow you to change loaded file? maybe not because need function to rerun file parse...
+        if (desiredFunction == 1)
+        {
+            findAll(data);
+        }
+        else if (desiredFunction == 2)
+            break;
+        else
+        {
+            std::cout << "ERROR! COULD NOT FIND MATCHING RESPONSE, PLEASE TRY AGAIN!\n";
+            continue;
+        }
+    }
 
     infile.close();
     return 0;
--- TextFileProcessor1/findAll.cpp
+++ TextFileProcessor1/findAll.cpp
@@ -3,19 +3,26 @@
 
 void findAll(const std::unordered_map<std::string, std::string>& data)
 {
+	std::cout << "List of keys:\n";
+	for (const auto& [key, value] : data) {
+		std::cout << key << "\n";
+	}
 
 	std::string desiredKey;
-	std::cout << "What key do you need?";
+	std::cout << "What key do you need?\n"; 
 	std::cin >> desiredKey;
 
-	std::vector<std::string> allItems;
+	auto item = data.find(desiredKey);
 
-	for (const auto& [str, value] : data) {
-		allItems.push_back(value);
+	if (item != data.end()) {
+		std::cout << item->second << std::endl;
 	}
-
-	for (const auto& value : allItems) {
-		std::cout << value << std::endl;
+	else {
+		std::cout << "Key not found!" << std::endl;
 	}
 	
+	
+
 }
+
+// Currently the desired key is not functioning and it is simply giving every part of the map when I only want it to print the part of map assigned to that key
